aaab0129a21620c83d8ffcd4cc05c05b01ca25e7
Amr <amr.khaledt1@gmail.com>
2025-06-20 22:31:18 +0300

    feat: Accept input argumetns from user

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
+try:
+    arg = sys.argv[1:]
+except IndexError:
+    sys.exit(1)
+
 response = client.models.generate_content(
     model='gemini-2.0-flash-001',
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents = arg
 )
 
 usage_metadata = response.usage_metadata
+
 print(response.text)
 print(f"Prompt tokens: {usage_metadata.prompt_token_count}\nResponse tokens: {usage_metadata.candidates_token_count}")
-# Prompt tokens: X
-# Response tokens: Y
