f905674c3c867db46db36e9d305ae1c81ac7aea3
Amr <amr.khaledt1@gmail.com>
2025-06-21 05:32:07 +0300

    refactor: update write_file.py

--- functions/write_file.py
+++ functions/write_file.py
@@ -2,17 +2,22 @@ import os
 
 
 def write_file(working_directory, file_path, content):
-    abs_working = os.path.abspath(working_directory)
-    full_path = os.path.abspath(os.path.join(working_directory, file_path))
-    
-    if not full_path.startswith(abs_working):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
-
-    if os.path.isfile(full_path):
+    if not os.path.exists(abs_file_path):
         try:
-            with open(full_path, "w", encoding="utf-8") as f:
-                f.write(content)
-                return f'Successfully wrote to "{full_path}" ({len(content)} characters written)'
-            
+            os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
         except Exception as e:
-            return f"Error: {e}"
+            return f"Error: creating directory: {e}"
+    if os.path.exists(abs_file_path) and os.path.isdir(abs_file_path):
+        return f'Error: "{file_path}" is a directory, not a file'
+    try:
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+    except Exception as e:
+        return f"Error: writing to file: {e}"
