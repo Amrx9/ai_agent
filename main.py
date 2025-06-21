3cd9ea0613a7fce6cbe2db2860b8f6ede3eec2ae
Amr <amr.khaledt1@gmail.com>
2025-06-21 17:34:56 +0300

    feat: func declartion

--- main.py
+++ main.py
@@ -3,11 +3,32 @@ import sys
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+from functions.get_files_info import get_files_info
 
 
 def main():
     load_dotenv()
-    
+
+    schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
+    )
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            ]
+    )
+        
     verbose = "--verbose" in sys.argv
     args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
@@ -21,7 +42,15 @@ def main():
     client = genai.Client(api_key=api_key)
 
     user_prompt = " ".join(args)
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
 
     if verbose:
         print(f"User prompt: {user_prompt}\n")
@@ -30,22 +59,34 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages, system_prompt, verbose)
+    result = generate_content(client, messages, system_prompt, available_functions, verbose)
+    print(result)
 
 
-def generate_content(client, messages, system_prompt, verbose):
+def generate_content(client, messages, system_prompt, available_functions, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        )
     )
 
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}\n")
 
-    print("Response:")
-    print(response.text)
+    # print("Response:")
+    # print(response.text)
+
+    res_fc = response.function_calls
+    if res_fc:
+        for func_call in res_fc:
+            return f"Calling function: {func_call.name}({func_call.args})"
+    else:
+        return response.text
+    
+
 
 if __name__ == "__main__":
     main()
